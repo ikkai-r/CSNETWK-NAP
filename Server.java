eca8f0cdfdfe512711a1ee971fe88ccf8611b5eb
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-11-21 02:23:15 +0800

    create processClientMessage

--- Server.java
+++ Server.java
@@ -1,13 +1,17 @@
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 public class Server {
+    private static final int PORT = 8080;
+    private static List<String> registeredAliases = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         //Listen to port
-        ServerSocket server = new ServerSocket(8080);
+        ServerSocket server = new ServerSocket(PORT);
 
         int count = 0;
         System.out.println("Server Started...");
 
-        while(true) {
+        while (true) {
             count++;
 
             //Accept requests and wait until client connects
@@ -17,5 +21,15 @@ public class Server {
             ServerApp sa = new ServerApp(serverClientSocket, count);
             sa.start();
         }
+    }
+    public static boolean isAliasRegistered(String alias) {
+        return registeredAliases.contains(alias);
+    }
+
+    public static void registerAlias(String alias) {
+        registeredAliases.add(alias);
+    }
+    public static void unregisterAlias(String alias) {
+        registeredAliases.remove(alias);
     }
 }
--- ServerApp.java
+++ ServerApp.java
@@ -4,6 +4,8 @@ import java.io.*;
 public class ServerApp extends Thread{
     Socket serverClientSocket;
     int clientNo;
+    String alias;
+    boolean isRunning = true;
 
     ServerApp(Socket inSocket, int ClientNo) {
         serverClientSocket = inSocket;
@@ -12,16 +14,64 @@ public class ServerApp extends Thread{
 
     public void run() {
         try {
-            DataInputStream inputStream = new DataInputStream(serverClientSocket.getInputStream());
-            DataOutputStream outputStream = new DataOutputStream(serverClientSocket.getOutputStream());
+            DataInputStream disReader = new DataInputStream(serverClientSocket.getInputStream());
+            DataOutputStream dosWriter = new DataOutputStream(serverClientSocket.getOutputStream());
 
-            String clientMessage = inputStream.readUTF();
-            String serverMessage = "";
+            String[] clientMessage;
 
-            outputStream.writeUTF("Client " + clientNo + " says " + clientMessage);
+            while (isRunning) {
+                clientMessage = disReader.readUTF().split(" ");
+                processClientMessage(clientMessage, dosWriter);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    private void processClientMessage(String[] message, DataOutputStream dosWriter) throws IOException {
+        switch (message[0]) {
+            case "/join" -> {
+                dosWriter.writeUTF("Connection to the File Exchange Server is Succesful");
+            }
+            case "/leave" -> {
+                System.out.println("Client " + clientNo + " is leaving.");
+                dosWriter.writeUTF("Connection closed. Thank you!");
+                isRunning = false;
+                if (alias != null) {
+                    Server.unregisterAlias(alias);
+                }
+                serverClientSocket.close();
+            }
+            case "/register" -> {
+                alias = message[1];
+                System.out.println("Client " + clientNo  + " is registering.");
+
+                if (!Server.isAliasRegistered(alias)) {
+                    Server.registerAlias(alias);
+                    dosWriter.writeUTF("Welcome " + alias);
+                } else {
+                    dosWriter.writeUTF("Error: Registration failed. Handle or alias already exists.");
+                }
+            }
+            case "/store" -> {
+                System.out.println("Client " + clientNo + " is storing.");
+                dosWriter.writeUTF("File stored successfully!");
+            }
+            case "/dir" -> {
+                System.out.println("Client " + clientNo + " is checking directory.");
+                dosWriter.writeUTF("Directory contents: file1.txt, file2.txt");
+            }
+            case "/get" -> {
+                System.out.println("Client " + clientNo + " is getting.");
+                dosWriter.writeUTF("File content: This is the content of the requested file.");
+            }
+            case "/?" -> {
+                System.out.println("Client " + clientNo + " is requesting help.");
+                dosWriter.writeUTF("Available commands: /register, /store, /dir, /get, /leave");
+            }
+            default -> dosWriter.writeUTF("Invalid command. Type /? for help.");
+        }
+    }
 }
+
+
